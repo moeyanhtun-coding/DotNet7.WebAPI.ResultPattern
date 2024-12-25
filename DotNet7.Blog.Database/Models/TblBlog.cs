100898d7646c0d70025444ce208a0092bead4c45
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-25 18:58:14 +0630

    feat : TblBlog Database

---
+++ DotNet7.Blog.Database/Models/TblBlog.cs
@@ -0,0 +1,16 @@
+﻿namespace DotNet7.Blog.Database.Models;
+
+public partial class TblBlog
+{
+    public int BlogId { get; set; }
+
+    public string BlogCode { get; set; } = null!;
+
+    public string BlogTitle { get; set; } = null!;
+
+    public string BlogAuthor { get; set; } = null!;
+
+    public string BlogContent { get; set; } = null!;
+
+    public bool DeleteFlag { get; set; } = false;
+}
---
+++ DotNet7.Blog.Domain/RequestModel/BlogRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNet7.Blog.Domain.RequestModel;
+
+public class BlogRequestModel
+{
+    public string BlogName { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+}
