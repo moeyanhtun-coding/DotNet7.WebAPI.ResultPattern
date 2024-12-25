251da418dd7688e081abdd89ea698720b54bab68
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-25 21:19:50 +0630

    feat : getBlogs in Controller

---
+++ DotNet7.Blog.WebAPI/Controllers/BlogController.cs
@@ -0,0 +1,30 @@
+﻿using DotNet7.Blog.Database.Models;
+using DotNet7.Blog.Database.RequestModel;
+using DotNet7.Blog.Database.ResponseModel;
+using DotNet7.Blog.Domain.Feature;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet7.Blog.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : BaseController
+{
+    private readonly BlogService _blogService = new BlogService();
+
+    [HttpGet]
+    public async Task<IActionResult> GetBlogs()
+    {
+        try
+        {
+            var model = await _blogService.GetBlogs();
+            return Execute(model);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+
+}
