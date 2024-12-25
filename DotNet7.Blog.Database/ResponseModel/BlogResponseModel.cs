288b96168ae1ec26c6da00684e082ea345b7dbc0
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-25 21:20:40 +0630

    add : create BlogListsResponeModel

---
+++ DotNet7.Blog.Database/ResponseModel/BlogResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using DotNet7.Blog.Database.Models;
+
+namespace DotNet7.Blog.Database.ResponseModel;
+
+public class BlogResponseModel
+{
+    public TblBlog Blog { get; set; }
+}
+
+public class BlogListResponseModel
+{
+    public List<TblBlog> Blogs { get; set; }
+}
--- DotNet7.Blog.Domain/ResponseModel/BlogResponseModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DotNet7.Blog.Domain.ResponseModel;
-
-public class BlogResponseModel
-{
-    public TblBlog Blog { get; set; }
-}
