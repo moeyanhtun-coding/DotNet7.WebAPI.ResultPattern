38c46bc8309c5c146977ca471c14981ec7c14ee3
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-25 21:27:34 +0630

    feat : add Global Using

--- DotNet7.Blog.Domain/GlobalUsings.cs
+++ DotNet7.Blog.Domain/GlobalUsings.cs
@@ -1,5 +1,6 @@
 ﻿// Global using directives
 
 global using DotNet7.Blog.Database.Models;
+global using DotNet7.Blog.Database.RequestModel;
 global using DotNet7.Blog.Database.ResponseModel;
 global using Microsoft.EntityFrameworkCore;
--- DotNet7.Blog.WebAPI/GlobalUsings.cs
+++ DotNet7.Blog.WebAPI/GlobalUsings.cs
@@ -1,3 +1,9 @@
 ﻿// Global using directives
 
+global using DotNet7.Blog.Database.Models;
+global using DotNet7.Blog.Database.RequestModel;
+global using DotNet7.Blog.Database.ResponseModel;
+global using DotNet7.Blog.Domain;
+global using DotNet7.Blog.Domain.Feature;
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
