a4f7f5e958263e4eeb76b11b829bcb3423297f85
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-25 18:57:39 +0630

    feat : Add AppDbContext

---
+++ DotNet7.Blog.Database/Models/AppDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet7.Blog.Database.Models;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblBlog> TblBlogs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            string connectionString = "Data Source=.;Inital Catalog=Blog;User ID=sa; Password=sasa@123; TrustServerCertificate=true";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblBlog>(entity =>
+        {
+            entity.HasKey(e => e.BlogId);
+
+            entity.ToTable("Tbl_Blog");
+
+            entity.Property(e => e.BlogAuthor).HasMaxLength(50);
+            entity.Property(e => e.BlogCode).HasMaxLength(50);
+            entity.Property(e => e.BlogTitle).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
