d7713edfef648cc7baeb9e68d9fbe5ea6d2f6e30
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-25 21:21:32 +0630

    feat : create GetBlogsList Service

--- DotNet7.Blog.Domain/Feature/BlogService.cs
+++ DotNet7.Blog.Domain/Feature/BlogService.cs
@@ -1,6 +1,39 @@
-﻿namespace DotNet7.Blog.Domain.Feature;
+﻿using DotNet7.Blog.Database.RequestModel;
+
+namespace DotNet7.Blog.Domain.Feature;
 
 public class BlogService
 {
-    
+    private readonly AppDbContext _dbContext = new AppDbContext();
+
+    public async Task<Result<BlogListResponseModel>> GetBlogs()
+    {
+        Result<BlogListResponseModel> model;
+        try
+        {
+            var lst = await _dbContext.TblBlogs.ToListAsync();
+
+            var data = new BlogListResponseModel
+            {
+                Blogs = lst
+            };
+
+            if (lst.Count == 0)
+            {
+                model = Result<BlogListResponseModel>.Success(data, "No blogs found");
+                goto Result;
+            }
+
+            model = Result<BlogListResponseModel>.Success(data, "Blogs found");
+
+            Result:
+            return model;
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+
+  
 }
