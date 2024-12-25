877814efa73f4a37fed134d79715de5e5e251af3
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-25 21:19:24 +0630

    feat : add Execute For Controller

---
+++ DotNet7.Blog.WebAPI/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using DotNet7.Blog.Domain;
+
+namespace DotNet7.Blog.WebAPI.Controllers;
+
+public class BaseController : ControllerBase
+{
+    public IActionResult Execute<T>(Result<T> model)
+    {
+        if (model.IsValidationError)
+            return BadRequest(model);
+        if (model.IsSystemError)
+            return StatusCode(500, model);
+        return Ok(model);
+    }
+}
