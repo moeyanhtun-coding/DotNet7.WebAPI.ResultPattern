c63d7d1bf2cbcb9edd5b5890531d6c625004b990
Moe Yan Htun <moeyan227970@gmail.com>
2025-01-03 22:32:33 +0630

    feat : register for DI

--- DotNet7.Blog.WebAPI/Program.cs
+++ DotNet7.Blog.WebAPI/Program.cs
@@ -1,7 +1,13 @@
+using DotNet7.Blog.Domain.Features;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+});
+builder.Services.AddScoped<BlogService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
